4d4c52f9e72d43361af977a461adc587afa5fa49
reneechou123 <reneechou123@gmail.com>
2018-10-29 10:57:21 -0400

    Update NAMESPACE and create.GO.sim.mat.R

--- R/create.GO.sim.mat.R
+++ R/create.GO.sim.mat.R
@@ -8,8 +8,8 @@ create.GO.sim.mat <- function(gtf.file.path, OrgDb, ont='BP', computeIC=FALSE, k
   geneids.filepath <- file.path(temp.folder, 'geneids.tsv')
   path <- paste(system.file(package='DGEclustering'), 'gene_ids_from_gtf.py', sep='/')
   system(paste(path,
-	       <, gtf.file.path,
-	       >, geneids.filepath))
+	       '<', gtf.file.path,
+	       '>', geneids.filepath))
   genes <- unique(read.table(geneids.filepath, header=FALSE, stringsAsFactors=FALSE)$V1)
 
 }
